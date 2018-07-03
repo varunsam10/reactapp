308ac7d6ad3a5eb8fe656ecb7f2b95dba84b858e
Varun Sam <varunsam1094@gmail.com>
2018-07-03 20:53:54 +0530

    Binding and Click on third Line added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
 import PersonClass from './Person/PersonClass'
 
 class App extends Component {
@@ -12,11 +11,12 @@ class App extends Component {
     ]
 }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was Clicked');
+    
     this.setState (    //We should not use this.state
     {    persons: [
-  {name :'Varun Sampath',age:'23'},
+  {name : newName ,age:'23'}, // WE SHOULD NOT PUT COMMA WHILE PASSING NAME
   {name :'Ragu',age:'23'},
   {name :'Avinash Ramesh',age:'24'}
 ]
@@ -30,12 +30,21 @@ class App extends Component {
       <div className="App">
        <h1> Hi, I am React App </h1>
        <p> This is my first Site in React </p>
-       <Person name="Varun" age="24"/>  
-       <button onClick ={this.switchNameHandler}>Switch button</button>
-       <PersonClass name={this.state.persons[0].name} age={this.state.persons[0].age}/>  
-       <PersonClass name={this.state.persons[1].name} age={this.state.persons[1].age}/>  
-       <PersonClass name={this.state.persons[2].name} age={this.state.persons[2].age}/>  
-      </div>
+      <button onClick ={this.switchNameHandler.bind(this, 'Varun!!')}>Switch button</button> 
+      
+       <PersonClass 
+       name={this.state.persons[0].name} 
+       age={this.state.persons[0].age}/>  
+
+       <PersonClass 
+       name={this.state.persons[1].name} 
+       age={this.state.persons[1].age} 
+       click={this.switchNameHandler.bind(this , 'Varun Sampath')}/>  
+
+       <PersonClass 
+       name={this.state.persons[2].name} 
+       age={this.state.persons[2].age}/>  
+       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi, I am React App Does it work'));
   }
--- src/Person/PersonClass.js
+++ src/Person/PersonClass.js
@@ -5,7 +5,7 @@ render(){
     return(
         //<p>This is an example of Class based Component </p>
         <div>
-        { <p>I am a Male and I am {this.props.name} I am {this.props.age} years old! </p> }
+        { <p onClick={this.props.click}>I am a Male and I am {this.props.name} I am {this.props.age} years old! </p> }
         <p> {this.props.children}</p>
         </div>
     );
