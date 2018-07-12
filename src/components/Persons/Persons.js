4c5be3ccccdffd48732e8bf100c73ff3bcb7f30d
Varun Sam <varunsam1094@gmail.com>
2018-07-12 19:33:28 +0530

    How Component creation Life cycle Added

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React , { Component }from 'react';
 
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map( ( person, index ) => {
-        return <Person
-          click={() => props.clicked( index )}
-          name={person.name}
-          age={person.age}
-          key={person.id}
-          changed={( event ) => props.changed( event, person.id )} />
-      } );
+class persons extends Component { 
+  constructor(props) {
+    super(props);
+    console.log('[Person.js] Inside Constructor',props);
+  }
+
+  componentWillMount() {
+    console.log('[Person.js] Inside ComponentWillMount',this.props)
+  }
+
+  componentDidMount(){
+    console.log('[Person.js] Inside componentDidMount')
+  }
+
+render() {
+    console.log('[Person.js] Inside Person Render');
+    return this.props.persons.map( ( person, index ) => {
+      return <Person
+        click={() => this.props.clicked( index )}
+        name={person.name}
+        age={person.age}
+        key={person.id}
+        changed={( event ) => this.props.changed( event, person.id )} />
+    } );
+      
+  }
+}
 
 export default persons;
